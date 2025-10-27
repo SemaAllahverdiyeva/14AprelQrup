fccd81c58f48849c0f6fa91038012bec9e70729f
SemaAllahverdiyeva <sema2013allahverdiyeva@gmail.com>
2025-10-27 20:57:12 +0400

    lesson2 - practice

--- Codes/javase/src/lesson1/Main.java
+++ Codes/javase/src/lesson1/Main.java
@@ -8,15 +8,15 @@ public class Main {
 
 		System.out.println("What is your name?");
 		String name = sc.nextLine();
-		System.out.println("I am " + name);
-		
+		System.out.println("My name is " + name);
+
 		System.out.println("What is your surname?");
 		String surname = sc.nextLine();
-		System.out.println(surname);
-		
+		System.out.println("My surname: " + surname);
+
 		System.out.println("How old are you?");
 		int age = sc.nextInt();
 		System.out.println("I am " + age);
-
+		sc.close();
 	}
 }
---
+++ Codes/javase/src/lesson2/VariablePractice.java
@@ -0,0 +1,20 @@
+package lesson2;
+
+public class VariablePractice {
+	public static void main(String[] args) {
+		int age = 20;
+		System.out.println("age: " + 20);
+		
+		boolean hasHome = true;
+		System.out.println("hasHome: " + hasHome);
+		
+		char studentGrade = 'A';
+		System.out.println("studentGrade: " + studentGrade);
+		
+		double productPrice = 3.8;
+		System.out.println("productPrice: " + productPrice);
+		
+		String studentName = "Yaqub";
+		System.out.println("studentName: " + studentName);
+	}
+}
