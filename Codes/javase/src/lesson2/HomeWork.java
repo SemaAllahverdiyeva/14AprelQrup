0975b54a9296acf8c9fa3949e89b7ef2219ae2fa
SemaAllahverdiyeva <sema2013allahverdiyeva@gmail.com>
2025-10-30 18:53:28 +0400

    lesson2- Home Work

---
+++ Codes/javase/src/lesson2/HomeWork.java
@@ -0,0 +1,20 @@
+package lesson2;
+
+public class HomeWork {
+	public static void main(String[] args) {
+		int age = 25;
+		System.out.println("age: " + age);
+		
+		boolean hasJob = false;
+		System.out.println("hasJob: " + hasJob);
+		
+		char studentGrade = 'C';
+		System.out.println("studentGrade: " + studentGrade);
+		
+		int carPrice = 250000;
+		System.out.println("carPrice: " + carPrice);
+		
+		String employeeName = "Məlik";
+		System.out.println("employeeName: " + employeeName);
+	}
+}
