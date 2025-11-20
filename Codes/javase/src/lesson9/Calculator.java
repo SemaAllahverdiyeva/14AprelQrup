55bfbeec7c8c5aafc19b8595c14262efcca0e7cf
SemaAllahverdiyeva <sema2013allahverdiyeva@gmail.com>
2025-11-20 20:18:40 +0400

    lesson 9

---
+++ Codes/javase/src/lesson9/Calculator.java
@@ -0,0 +1,7 @@
+package lesson9;
+
+public class Calculator {
+	public double average(int a, int b) {
+		return (a + b) / 2;
+	}
+}
---
+++ Codes/javase/src/lesson9/Main.java
@@ -0,0 +1,9 @@
+package lesson9;
+
+public class Main {
+	public static void main(String[] args) {
+		Calculator c = new Calculator();
+		double average = c.average(7, 13);
+		System.out.println(average);
+	}
+}
