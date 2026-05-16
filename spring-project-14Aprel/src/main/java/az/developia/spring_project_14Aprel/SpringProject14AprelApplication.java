e77bd114a1f434df51892a3e77b47dba60a36075
SemaAllahverdiyeva <sema2013allahverdiyeva@gmail.com>
2026-05-16 17:18:50 +0400

    spring

---
+++ spring-project-14Aprel/src/main/java/az/developia/spring_project_14Aprel/SpringProject14AprelApplication.java
@@ -0,0 +1,13 @@
+package az.developia.spring_project_14Aprel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProject14AprelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProject14AprelApplication.class, args);
+	}
+
+}
--- spring-project-14Aprel/src/test/java/az/developia/spring_project_14Aprel/SpringProject14AprelApplicationTests.java
+++ spring-project-14Aprel/src/test/java/az/developia/spring_project_14Aprel/SpringProject14AprelApplicationTests.java
@@ -1,10 +1,10 @@
-package az.developia.spring_project_14aprel;
+package az.developia.spring_project_14Aprel;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringProject14aprelApplicationTests {
+class SpringProject14AprelApplicationTests {
 
 	@Test
 	void contextLoads() {
